61e8c400a5820e743af541f5d47656aa74f7f697
Vlad12022009 <84780546+Vlad12022009@users.noreply.github.com>
2024-01-18 17:35:24 +0300

    Add files via upload

---
+++ request2.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+
+from bs4 import BeautifulSoup
+while True:
+    query = sys.argv[1] if len(sys.argv) > 1 else input('Введите тип вашего аватара: ')
+    url = f'https://www.kiddle.co/s.php?q={query}'
+
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    #print(dir(soup))
+    for raw_img in soup.find_all('img'):
+        link = raw_img.get('src')
+        if link and link.startswith('https'):
+            response = requests.get(link)
+            with open("./today_avatar.jpg", 'wb') as f:
+                f.write(response.content)
+            break
+    else:
+        print("Аватар не найден - today_avatar.jpg")
